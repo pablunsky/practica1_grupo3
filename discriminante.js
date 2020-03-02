bc634768642d1f86ad883e4d510a5a7e6f51b793
Jorge2808 <60764752+Jorge2808@users.noreply.github.com>
2020-03-01 20:22:03 -0600

    Agregada funcionalidad Numero Absoluto - 201503421

--- discriminante.js
+++ discriminante.js
@@ -2,3 +2,22 @@ function disc(a, b, c) {
     const dis = Math.pow(b,2) - (4*a*c);
     return dis;
 }
+
+function numeroAbsoluto(numero)
+{
+    if(!isNaN(numero))
+    {
+        console.log('Numero Ingresado: '+numero)
+        if(numero < 0)
+        {
+            numero = numero * -1;
+        }
+        console.log('Numero Absoluto: '+numero);
+        return numero;
+    }
+    else
+    {
+        console.log('El Valor ingresado no es un numero');
+        return 0;
+    } 
+}
