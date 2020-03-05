f90c80e184fed0ec155b784cf72a52d85f65fb32
pablunsky <hpablo677@gmail.com>
2020-03-04 19:48:09 -0600

    Validacion en multiplicacion.

--- MultiplicacionDivision.js
+++ MultiplicacionDivision.js
@@ -1,4 +1,8 @@
 function multiplicar(n1,n2){
+    if(isNan(n1) || isNaN(n2)){
+        console.log('No se han ingresado numeros.');
+        return 0;
+    }
   return (parseInt(n1) * parseInt(n2));
 }
 
