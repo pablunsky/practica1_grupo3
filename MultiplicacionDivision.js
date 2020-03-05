01b3fbbff332e408c3b441b28e47c584fd8fdc43
Denis Hernandez <kandor95@gmail.com>
2020-03-04 21:02:03 -0600

    verificacion de dato numerico

--- MultiplicacionDivision.js
+++ MultiplicacionDivision.js
@@ -13,9 +13,14 @@ function multiplicar(n1,n2){
 }
 
 function dividir(n1,n2){
-  if (n2!=0){
-    return (parseInt(n1) / parseInt(n2));
+  if(Number.isInteger(n1)&&Number.isInteger(n1)){
+    if (n2!=0){
+      return (parseInt(n1) / parseInt(n2));
+    }
+    console.log('dividendo no puede ser 0');
+    return 0;
+  }else{
+    console.log('Uno de los datos no es un Numero');
+    return 0;
   }
-  console.log('dividendo no puede ser 0');
-  return 0;
 }
