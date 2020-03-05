4d61a9b91ee33a02ca369fc19ef4b1fcc7152486
Denis Hernandez <kandor95@gmail.com>
2020-03-04 20:58:40 -0600

    verificar que los datos sean numericos

--- MultiplicacionDivision.js
+++ MultiplicacionDivision.js
@@ -3,7 +3,13 @@ function multiplicar(n1,n2){
         console.log('No se han ingresado numeros.');
         return 0;
     }
-  return (parseInt(n1) * parseInt(n2));
+    if(Number.isInteger(n1)&&Number.isInteger(n1)){
+      return (parseInt(n1) * parseInt(n2));
+    }else{
+        console.log('Uno de los datos no es un Numero');
+        return 0;
+    }
+  
 }
 
 function dividir(n1,n2){
