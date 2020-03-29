bc50a4d5da0e4b6317211482626f0c633c6912d9
Carlos Cordon <carlos.edu.cordon@gmail.com>
2020-03-29 16:33:13 -0600

    style: Comentario funcion discriminante.

--- discriminante.js
+++ discriminante.js
@@ -3,6 +3,7 @@ function disc(a, b, c, err) {
         console.log('Las valores deben ser numeros positivos');
         return -1;
     }
+    // Comentario calificacion 201504427
     const linf = Math.pow(b,2) - (4*a*c) - inf;
     const lsup = Math.pow(b,2) - (4*a*c) + inf;
     return {inferior: linf, superior : lsup};
